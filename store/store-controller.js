cc9805d79ae1fedb613c0b43969fe454134a7f37
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-05-21 12:18:55 -0700

    now filters, needs array index correction

--- store/store-controller.js
+++ store/store-controller.js
@@ -5,51 +5,59 @@ angular
   .module('shoppingApp')
   .controller('StoreController', StoreController);
 
-  function StoreController () {
+StoreController.$inject = ['$timeout', 'InventoryService'];
+
+  function StoreController ($timeout, InventoryService) {
     var vm = this;
+    vm.items = InventoryService.getTeas();
     vm.view = 'store!';
-    vm.quantity = 0;
-    vm.subtotal = '$$$$$$';
-    vm.quantityOn = true;
-    vm.editQuantity = function () {
-      vm.quantityOn = !vm.quantityOn;
+    vm.cart = [];
+    vm.cartSubtotal = 0;
+    vm.cartQuantity = 0;
+    vm.tooMany = false;
+    vm.categories = [];
+    vm.orderByCategory = '';
+
+    vm.filterOut = function(item) {
+      if((item.categories.indexOf(vm.orderByCategory) > -1) ||
+          vm.orderByCategory === ''){
+        return true;
+      }else{
+        return false;
+      }
     }
-    vm.addQuantity = function(number) {
-      vm.quantity = number;
-      vm.quantityOn = true;
-      vm.quantity = 0;
+
+    for(var i = 0; i < vm.items.length; i++) {
+      vm.items[i].quantityInCart = 0;
+      vm.items[i].quantityToAdd = 1;
+      vm.items[i].quantityInStock = 5;
+      if(vm.items[i].quantityInStock > 0) {
+        vm.items[i].inStock = true;
+      }
+      for(var j = 0; j < vm.items[i].categories.length; j++) {
+        if(vm.categories.indexOf(vm.items[i].categories[j]) < 0) {
+          vm.categories.push(vm.items[i].categories[j]);
+        }
+      }
     }
-    vm.items = [{
-        "_id": "55c8ee82152165d244b98300",
-        "name": "Bayard stew",
-        "ingredients": "concentrated gluten, jewelry, dill, beetle nut, toast",
-        "caffeineScale": 244,
-        "price": 1540,
-        "inStock": true,
-        "rating": 1,
-        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32664_d?$cimg$",
-        "__v": 0,
-        "categories": [ "dark", "cold"]
-    },
-    {
-      'one': 'first',
-      'two': 'second',
-      'three': 'third',
-      'four': 'fourth',
-      'five': 'fifth',
-      'imageUrl': 'http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$',
-      'name' : 'thereisnoname',
-      'categories': ['cat1', 'cat2', 'cat3', 'cat4']
-    },
-    {
-      'one': 'first',
-      'two': 'second',
-      'three': 'third',
-      'four': 'fourth',
-      'five': 'fifth',
-      'imageUrl': 'http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$',
-      'name' : 'thereisnoname',
-      'categories': ['cat1', 'cat2', 'cat3', 'cat4']
-    }]
+
+    vm.addQuantity = function(index) {
+      if((vm.items[index].quantityInCart +
+        vm.items[index].quantityToAdd) > vm.items[index].quantityInStock){
+          vm.tooMany = true;
+          $timeout(function(){
+            vm.tooMany = false}, 3000);
+          return;
+        }
+      vm.items[index].quantityInCart += vm.items[index].quantityToAdd;
+      vm.cartQuantity += vm.items[index].quantityToAdd;
+      vm.items[index].quantityToAdd = 1;
+      vm.cartSubtotal += vm.items[index].price * vm.items[index].quantityInCart;
+    }
+
+    vm.editQuantity = function (index) {
+      // vm.items[index].quantityOn = !vm.items[index].quantityOn;
+    }
+
   }
 })();
